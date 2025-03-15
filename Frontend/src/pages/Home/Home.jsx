bfa43f941ae8e801b8bffa120f3cd57d6d33c147
Asaduzzaman04 <asadchy152@gmail.com>
2025-03-15 19:40:55 +0600

    Resolved merge conflicts by using dev branch code

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import AboutUs from '../AboutUs/AboutUs';
+import React, { useMemo } from "react";
+import Hero from "../../components/Home/Hero";
+import AspireSection from "../../components/Home/AspireSection";
+import CareerJourney from "./../../components/Home/CareerJourney";
+import aspireSectionData from "../../data/home.aspireSection.json";
+import CareerJourneysectiondata from "../../data/home.careenJourney.json";
 
 const Home = () => {
-    return (
-        <div>
-           
-        </div>
-    );
+  const memoizedAspireData = useMemo(() => aspireSectionData, []);
+  const memoizedCareerJourneydata = useMemo(() => CareerJourneysectiondata, []);
+
+  return (
+    <section className="bg-primary-color mt-24 py-8">
+      {/* hero-section */}
+      <Hero />
+      <AspireSection aspireSectionData={memoizedAspireData} />
+      <CareerJourney CareerJourneydata={memoizedCareerJourneydata} />
+    </section>
+  );
 };
 
 export default Home;
