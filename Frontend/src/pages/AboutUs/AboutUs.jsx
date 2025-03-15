0610bea738701568f09dcd175369a4ccf89a3834
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2025-03-15 11:07:08 +0600

    responsive mission section

--- Frontend/src/pages/AboutUs/AboutUs.jsx
+++ Frontend/src/pages/AboutUs/AboutUs.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Header from './Header/Header';
 import Banner from './Banner/Banner';
 import Quote from './quote/quote';
+import Mission from './Misson/Misson';
 
 const AboutUs = () => {
     return (
-        <div  style={{ fontFamily: "'Sen', sans-serif" }} className='bg-[#EFF2FA]'>
+        <div  style={{ fontFamily: "'Sen', sans-serif" }} className='bg-[#EFF2FA] '>
             <Header></Header>
             <Banner></Banner>
             <Quote></Quote>
+            <Mission></Mission>
         </div>
     );
 };
